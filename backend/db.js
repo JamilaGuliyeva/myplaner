d82b447a65b07a2e31214e7c642cd05388081ec0
JamilaGuliyeva <snin_emailin@gmail.com>
2026-03-23 15:51:28 +0400

    db connection fix

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
+// Render-də "Environment Variable" varsa ondan istifadə et, yoxdursa localhost-a bax
+const isProduction = process.env.DATABASE_URL;
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'planner_db', 
-  password: 'CQGq0806', 
-  port: 5432,
+  connectionString: isProduction ? process.env.DATABASE_URL : "postgresql://postgres:CQGq0806@localhost:5432/planner_db",
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
