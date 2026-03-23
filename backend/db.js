3ecd7e011aaa2ff23f949f6972e2b686e415b43e
JamilaGuliyeva <snin_emailin@gmail.com>
2026-03-23 17:23:37 +0400

    database direct link fix

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,6 @@
 const { Pool } = require('pg');
-
 const pool = new Pool({
-  connectionString: "postgres://myplanner_user:Ez0TltJCBJrtwHaKCATMXci9ar2Z@dpg-d707c22a214c73e53pq0-a.oregon-postgres.render.com/myplanner",
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
-
 module.exports = pool;
