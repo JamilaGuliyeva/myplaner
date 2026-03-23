4d71a850affbab7f3cc8e9bd2d6cd0f4230d07d9
JamilaGuliyeva <snin_emailin@gmail.com>
2026-03-23 16:18:27 +0400

    database fix for render

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
-// Render-də "Environment Variable" varsa ondan istifadə et, yoxdursa localhost-a bax
-const isProduction = process.env.DATABASE_URL;
-
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : "postgresql://postgres:CQGq0806@localhost:5432/planner_db",
-  ssl: isProduction ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
